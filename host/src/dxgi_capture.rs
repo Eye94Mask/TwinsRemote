0662398c7dcbd656b89dac9134cf02892b26a1aa
meu <hiroki.1106su@gmail.com>
2026-03-12 16:52:37 +0900

    画面共有の安定性向上

--- host/src/dxgi_capture.rs
+++ host/src/dxgi_capture.rs
@@ -1,7 +1,7 @@
 use anyhow::{Result, anyhow};
 use webrtc::ice::stats;
 use windows::{
-    core::Interface,
+    core::*,
     Win32:: {
         Foundation::*,
         Graphics::{
@@ -24,7 +24,7 @@ pub struct DxgiCapture {
 }
 
 impl DxgiCapture {
-    pub fn new(width: u32, height: u32) -> Result<Self> {
+    pub fn new() -> Result<Self> {
         unsafe {
             // -------------------------------
             // Create D3D11 Device
@@ -54,6 +54,12 @@ impl DxgiCapture {
 
             let duplication = output1.DuplicateOutput(&device)?;
 
+            let mut desc = DXGI_OUTDUPL_DESC::default();
+            duplication.GetDesc(&mut desc);
+
+            let width = desc.ModeDesc.Width;
+            let height = desc.ModeDesc.Height;
+
             Ok(Self {
                 device,
                 context,
@@ -64,16 +70,22 @@ impl DxgiCapture {
         }
     }
     
-    pub fn capture_frame(&mut self) -> Result<(Vec<u8>, u32)> {
+    pub fn capture_frame(&mut self) -> Result<Option<Vec<u8>>> {
         unsafe {
             let mut frame_info = DXGI_OUTDUPL_FRAME_INFO::default();
             let mut resource = None;
 
-            self.duplication.AcquireNextFrame(
-                0,
-                &mut frame_info,
-                &mut resource
-            )?;
+            match self.duplication.AcquireNextFrame(16, &mut frame_info, &mut resource) {
+                Ok(_) => {}
+                Err(e) => {
+                    if e.code() == DXGI_ERROR_WAIT_TIMEOUT {
+                        return Ok(None);
+                    } 
+                    else {
+                        return Err(anyhow!("frame acquire failed"));
+                    }
+                }
+            }
 
             let resource = resource.ok_or(anyhow!("No Resource"))?;
             let texture: ID3D11Texture2D = resource.cast()?;
@@ -87,21 +99,14 @@ impl DxgiCapture {
             desc.MiscFlags = 0;
 
             let mut staging: Option<ID3D11Texture2D> = None;
+            self.device.CreateTexture2D(&desc, None, Some(&mut staging))?;
 
-            self.device.CreateTexture2D(
-                &desc,
-                None,
-                Some(&mut staging)
-            )?;
             let staging = staging.unwrap();
 
-            // ID3D11Texture2D -> ID3D11Resource
-            let staging_resource: ID3D11Resource = staging.cast()?;
-            let texture_resource: ID3D11Resource = texture.cast()?;
-
             self.context.CopyResource(&staging, &texture);
-            
+
             let mut mapped = D3D11_MAPPED_SUBRESOURCE::default();
+
             self.context.Map(
                 &staging,
                 0,
@@ -110,21 +115,26 @@ impl DxgiCapture {
                 Some(&mut mapped)
             )?;
 
-            let row_pitch = mapped.RowPitch;
+            let row_pitch = mapped.RowPitch as usize;
             // My Desktop Capture Frame Format (BGRA: width x height x 4)
-            let size = (row_pitch * self.height) as usize;
+            let mut frame = vec![0u8; (self.width * self.height * 4) as usize];
 
-            let data = std::slice::from_raw_parts(
-                mapped.pData as *const u8,
-                size
-            );
+            for y in 0..self.height as usize {
+                let src = std::slice::from_raw_parts(
+                    (mapped.pData as *const u8).add(y * row_pitch),
+                    (self.width * 4) as usize
+                );
 
-            let buffer = data.to_vec();
+                let dst_offset = y * (self.width * 4) as usize;
+
+                frame[dst_offset..dst_offset + (self.width * 4) as usize].copy_from_slice(src);
+            }
 
             self.context.Unmap(&staging, 0);
+
             self.duplication.ReleaseFrame()?;
 
-            Ok((buffer, row_pitch))
+            Ok(Some(frame))
         }
     }
 
--- host/src/main.rs
+++ host/src/main.rs
@@ -4,8 +4,6 @@ mod webrtc_sender;
 mod consts;
 
 use std::sync::Arc;
-use std::process::ChildStdin;
-use std::io::Read;
 
 use anyhow::Result;
 
@@ -13,49 +11,40 @@ use tokio::time::{sleep, Duration};
 
 use bytes::Bytes;
 
-use webrtc::api::APIBuilder;
-use webrtc::peer_connection::configuration::RTCConfiguration;
-use webrtc::rtp_transceiver::rtp_codec::RTCRtpCodecCapability;
-use webrtc::track::track_local::track_local_static_rtp::TrackLocalStaticRTP;
-use webrtc::track::track_local::TrackLocalWriter;
-
 use rustls::crypto::ring::default_provider;
 
 use dxgi_capture::DxgiCapture;
 use ffmpeg::FfmpegEncoder;
 use webrtc_sender::WebRtcSender;
-use crate::consts::{FPS_MILLIS, MY_MONITOR_WIDTH, MY_MONITOR_HEIGHT};
+use crate::consts::FPS_MILLIS;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     default_provider().install_default().expect("install rustls crypto provider");
     println!("Starting Remote Play Host");
     
-    let mut capture = DxgiCapture::new(MY_MONITOR_WIDTH, MY_MONITOR_HEIGHT)?;
-    let mut encoder = FfmpegEncoder::new(MY_MONITOR_WIDTH, MY_MONITOR_HEIGHT)?;
+    let mut capture = DxgiCapture::new()?;
+    let (width, height) = capture.resolution();
+
+    let mut encoder = FfmpegEncoder::new(width, height)?;
     
     let webrtc = Arc::new(WebRtcSender::new().await?);
     let webrtc_clone = webrtc.clone();
     
     encoder.send_nal(webrtc_clone).await?;
-    let (width, height) = capture.resolution();
     
     tokio::spawn(async move {
         loop {
-            if let Ok((data, row_pitch)) = capture.capture_frame() {
-                let mut packet = Vec::with_capacity((width * height * 4) as usize);
-
-                for y in 0..height as usize {
-                    let start = y * row_pitch as usize;
-                    let end = start + (width as usize * 4);
-
-                    packet.extend_from_slice(&data[start..end]);
-
+            match capture.capture_frame() {
+                Ok(Some(frame)) => {
+                    if encoder.encode(&frame).await.is_err() {
+                        eprintln!("encoder stdin closed");
+                        break;
+                    }
                 }
-                
-                if encoder.encode(&packet).await.is_err() {
-                    eprintln!("stdin write failed");
-                    break;
+                Ok(None) => {}
+                Err(e) => {
+                    eprintln!("capture error: {:?}", e);
                 }
             }
         }
