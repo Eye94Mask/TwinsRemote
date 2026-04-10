2f790b9f2fd2e0e98ee3f23fb8b529d888df85a2
meu <hiroki.1106su@gmail.com>
2026-04-10 20:25:37 +0900

    実行時にアプリを指定できるように変更

--- ExcludeAppsAudio/ExcludeAppsAudio/ExcludeAppsAudio.cpp
+++ ExcludeAppsAudio/ExcludeAppsAudio/ExcludeAppsAudio.cpp
@@ -1,80 +1,125 @@
 ﻿// ApplicationLoopback.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <Windows.h>
+#include <windows.h>
 #include <iostream>
-#include "LoopbackCapture.h"
+#include <stdio.h>
+#include <tchar.h>
+#include <psapi.h>
+#include <vector>
+//#include "LoopbackCapture.h"
+
+using namespace std;
 
 void usage()
 {
     std::wcout <<
-        L"Usage: ApplicationLoopback <pid> <includetree|excludetree> <outputfilename>\n"
-        L"\n"
-        L"<pid> is the process ID to capture or exclude from capture\n"
-        L"includetree includes audio from that process and its child processes\n"
-        L"excludetree includes audio from all processes except that process and its child processes\n"
-        L"<outputfilename> is the WAV file to receive the captured audio (10 seconds)\n"
-        L"\n"
-        L"Examples:\n"
-        L"\n"
-        L"ApplicationLoopback 1234 includetree CapturedAudio.wav\n"
-        L"\n"
-        L"  Captures audio from process 1234 and its children.\n"
-        L"\n"
-        L"ApplicationLoopback 1234 excludetree CapturedAudio.wav\n"
-        L"\n"
-        L"  Captures audio from all processes except process 1234 and its children.\n";
+        L"Usage: ExcludeAppsAudio <AppName> <includetree|excludetree> <outputfilename>\n"
+        L"\n";
 }
 
-int wmain(int argc, wchar_t* argv[])
+bool isTheSameWord(TCHAR* procName, TCHAR* targetName) {
+    int sizeOfProcName =  sizeof(procName);
+    int sizeOfTargetName = sizeof(targetName);
+
+    if (sizeOfProcName != sizeOfTargetName) return false;
+
+    for (int i = 0; i < sizeOfProcName; ++i) {
+        if (procName[i] != targetName[i]) return false;
+    }
+
+    return true;
+}
+
+void GetPidFromAppName(TCHAR* targetName, vector<DWORD>& ret) {
+    
+    DWORD allProc[1024];
+    DWORD cbNeeded;
+    int nProc;
+    int i;
+
+    // PID一覧を取得
+    if (!EnumProcesses(allProc, sizeof(allProc), &cbNeeded)) return;
+
+    nProc = cbNeeded / sizeof(DWORD);
+
+    for (i = 0; i < nProc; i++) {
+        TCHAR procName[MAX_PATH] = TEXT("<unknown>");
+
+        HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION |
+            PROCESS_VM_READ,
+            FALSE, allProc[i]);
+
+        // プロセス名を取得
+        if (NULL != hProcess) {
+            HMODULE hMod;
+            DWORD cbNeeded;
+
+            if (EnumProcessModules(hProcess, &hMod, sizeof(hMod),
+                &cbNeeded)) {
+                GetModuleBaseName(hProcess, hMod, procName,
+                    sizeof(procName) / sizeof(TCHAR));
+            }
+        }
+
+        if (isTheSameWord(procName, targetName)) {
+            ret.push_back(allProc[i]);
+            // プロセス名とPIDを表示
+            _tprintf(TEXT("%s  (PID: %u)\n"), procName, allProc[i]);
+        }
+
+        CloseHandle(hProcess);
+    }
+}
+
+int _tmain(int argc, TCHAR* argv[])
 {
-    if (argc != 4)
-    {
-        usage();
-        return 0;
-    }
+    // if (argc < 4)
+    // {
+    //     usage();
+    //     return 0;
+    // }
 
-    DWORD processId = wcstoul(argv[1], nullptr, 0);
-    if (processId == 0)
-    {
-        usage();
-        return 0;
-    }
+    // DWORD appName = wcstoul(argv[1], nullptr, 0);
+    // if (processId == 0)
+    // {
+    //     usage();
+    //     return 0;
+    // }
 
-    bool includeProcessTree;
-    if (wcscmp(argv[2], L"includetree") == 0)
-    {
-        includeProcessTree = true;
-    }
-    else if (wcscmp(argv[2], L"excludetree") == 0)
-    {
-        includeProcessTree = false;
-    }
-    else
-    {
-        usage();
-        return 0;
-    }
+    // bool includeProcessTree = false;
 
-    PCWSTR outputFile = argv[3];
+    // PCWSTR outputFile = argv[2];
 
-    CLoopbackCapture loopbackCapture;
-    HRESULT hr = loopbackCapture.StartCaptureAsync(processId, includeProcessTree, outputFile);
-    if (FAILED(hr))
-    {
-        wil::unique_hlocal_string message;
-        FormatMessageW(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_ALLOCATE_BUFFER, nullptr, hr,
-            MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (PWSTR)&message, 0, nullptr);
-        std::wcout << L"Failed to start capture\n0x" << std::hex << hr << L": " << message.get() << L"\n";
-    }
-    else
-    {
-        std::wcout << L"Capturing 10 seconds of audio." << std::endl;
-        Sleep(10000);
+    // CLoopbackCapture loopbackCapture;
+    // HRESULT hr = loopbackCapture.StartCaptureAsync(processId, includeProcessTree, outputFile);
+    // if (FAILED(hr))
+    // {
+    //     wil::unique_hlocal_string message;
+    //     FormatMessageW(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_ALLOCATE_BUFFER, nullptr, hr,
+    //         MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (PWSTR)&message, 0, nullptr);
+    //     std::wcout << L"Failed to start capture\n0x" << std::hex << hr << L": " << message.get() << L"\n";
+    // }
+    // else
+    // {
+    //     std::wcout << L"Capturing 10 seconds of audio." << std::endl;
+    //     Sleep(10000);
 
-        loopbackCapture.StopCaptureAsync();
+    //     loopbackCapture.StopCaptureAsync();
 
-        std::wcout << L"Finished.\n";
+    //     std::wcout << L"Finished.\n";
+    // }
+
+    // 結果を入れるベクタ
+    vector<DWORD> pids;
+    
+    TCHAR appName[MAX_PATH];
+
+    _tcsncpy_s(appName, argv[1], _TRUNCATE);
+    GetPidFromAppName(appName, pids);
+
+    for (const DWORD pid : pids) {
+        cout << pid << endl;
     }
 
     return 0;
