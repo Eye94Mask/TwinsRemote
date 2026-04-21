b1464dbea005a72a21420233e2d10de1ebc134c5
meu <hiroki.1106su@gmail.com>
2026-04-21 17:11:18 +0900

    DualShock4 / DualSense 対応

--- twins_remote_host/src/consts.rs
+++ twins_remote_host/src/consts.rs
@@ -1,25 +1,12 @@
 use std::time::Duration;
 
-pub const FPS_MILLIS: u64 = 16;     // 33 = 30fps, 16 = 60fps
-pub const TIMEOUT_MILLIS: u32 = 16;
-pub const LOOP_MILLIS: u64 = 33;    // For WebRTC
-
-pub const FPS: u64 = 60;
-pub const FRAME_DURATION_MS: u64 = 1000 / FPS;
-
 // 60fps
 pub const VIDEO_FRAME_DURATION: Duration = Duration::from_micros(16_667);
 
-pub const MTU: usize = 1200;
-pub const AUDIO_FRAME: usize = 960 * 2;
-
-pub const VIRTUAL_DESKTOP_COORD: i32 = 65535;
-
-pub const MY_MONITOR_WIDTH:  u32 = 3840;
-pub const MY_MONITOR_HEIGHT: u32 = 2160;
-
-pub const FULL_HD_WIDTH:     u32 = 1920;
-pub const FULL_HD_HEIGHT:    u32 = 1080;
+// PS Controller
+pub const SONY_VENDOR_ID: u16    = 0x054C;
+pub const DS4_PRODUCT_ID: u16    = 0x05C4;
+pub const DSENSE_PRODUCT_ID: u16 = 0x09CC;
 
 // -------------------------------
 // Input Types
@@ -40,11 +27,6 @@ pub const THRESHOLD_OF_MOUSE_TIME_MILLIS: u64 = 500;
 // -------------------------------
 // Controller Buttons
 // -------------------------------
-pub const BUTTON_A: u16     = 0x1000;
-pub const BUTTON_B: u16     = 0x2000;
-pub const BUTTON_X: u16     = 0x4000;
-pub const BUTTON_Y: u16     = 0x8000;
-
 pub const BUTTON_UP: u16    = 0x0001;
 pub const BUTTON_DOWN: u16  = 0x0002;
 pub const BUTTON_LEFT: u16  = 0x0004;
@@ -54,5 +36,13 @@ pub const BUTTON_START: u16 = 0x0010;
 pub const BUTTON_BACK: u16  = 0x0020;
 pub const BUTTON_LS: u16    = 0x0040;
 pub const BUTTON_RS: u16    = 0x0080;
+
 pub const BUTTON_LB: u16    = 0x0100;
 pub const BUTTON_RB: u16    = 0x0200;
+pub const BUTTON_GUIDE: u16 = 0x0400;
+
+pub const BUTTON_A: u16     = 0x1000;
+pub const BUTTON_B: u16     = 0x2000;
+pub const BUTTON_X: u16     = 0x4000;
+pub const BUTTON_Y: u16     = 0x8000;
+
