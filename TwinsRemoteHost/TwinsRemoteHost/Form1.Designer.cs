ac0fbcc7ef6813996485ff040e9b2da0c547da17
meu <hiroki.1106su@gmail.com>
2026-04-23 23:19:28 +0900

    ホストフォームからの映像配信対応

--- TwinsRemoteHost/TwinsRemoteHost/Form1.Designer.cs
+++ TwinsRemoteHost/TwinsRemoteHost/Form1.Designer.cs
@@ -28,92 +28,160 @@
         /// </summary>
         private void InitializeComponent()
         {
-            streamModeDesc = new TextBox();
-            radioBalanced = new RadioButton();
-            radioQuality = new RadioButton();
-            radioStable = new RadioButton();
-            radioMobile = new RadioButton();
+            labelMode = new Label();
+            labelSessionId = new Label();
+            labelStatusTitle = new Label();
+            labelStatusValue = new Label();
+            comboBoxMode = new ComboBox();
+            textBoxSessionId = new TextBox();
+            buttonStart = new Button();
+            buttonAudioOn = new Button();
+            buttonAudioOff = new Button();
+            buttonStop = new Button();
+            textBoxLog = new TextBox();
             SuspendLayout();
             // 
-            // streamModeDesc
+            // labelMode
             // 
-            streamModeDesc.BackColor = SystemColors.HighlightText;
-            streamModeDesc.Font = new Font("メイリオ", 14F, FontStyle.Regular, GraphicsUnit.Point, 128);
-            streamModeDesc.Location = new Point(120, 70);
-            streamModeDesc.Name = "streamModeDesc";
-            streamModeDesc.Size = new Size(150, 49);
-            streamModeDesc.TabIndex = 4;
+            labelMode.AutoSize = true;
+            labelMode.Font = new Font("メイリオ", 14F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            labelMode.Location = new Point(98, 44);
+            labelMode.Name = "labelMode";
+            labelMode.Size = new Size(98, 42);
+            labelMode.TabIndex = 0;
+            labelMode.Text = "label1";
             // 
-            // radioBalanced
+            // labelSessionId
             // 
-            radioBalanced.AutoSize = true;
-            radioBalanced.Font = new Font("メイリオ", 14F, FontStyle.Regular, GraphicsUnit.Point, 128);
-            radioBalanced.Location = new Point(120, 150);
-            radioBalanced.Name = "radioBalanced";
-            radioBalanced.Size = new Size(165, 46);
-            radioBalanced.TabIndex = 5;
-            radioBalanced.TabStop = true;
-            radioBalanced.Text = "Balanced";
-            radioBalanced.UseVisualStyleBackColor = true;
+            labelSessionId.AutoSize = true;
+            labelSessionId.Font = new Font("メイリオ", 14F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            labelSessionId.Location = new Point(577, 44);
+            labelSessionId.Name = "labelSessionId";
+            labelSessionId.Size = new Size(98, 42);
+            labelSessionId.TabIndex = 1;
+            labelSessionId.Text = "label1";
             // 
-            // radioQuality
+            // labelStatusTitle
             // 
-            radioQuality.AutoSize = true;
-            radioQuality.Font = new Font("メイリオ", 14F, FontStyle.Regular, GraphicsUnit.Point, 128);
-            radioQuality.Location = new Point(120, 250);
-            radioQuality.Name = "radioQuality";
-            radioQuality.Size = new Size(138, 46);
-            radioQuality.TabIndex = 6;
-            radioQuality.TabStop = true;
-            radioQuality.Text = "Quality";
-            radioQuality.UseVisualStyleBackColor = true;
+            labelStatusTitle.AutoSize = true;
+            labelStatusTitle.Font = new Font("メイリオ", 14F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            labelStatusTitle.Location = new Point(98, 358);
+            labelStatusTitle.Name = "labelStatusTitle";
+            labelStatusTitle.Size = new Size(98, 42);
+            labelStatusTitle.TabIndex = 2;
+            labelStatusTitle.Text = "label1";
             // 
-            // radioStable
+            // labelStatusValue
             // 
-            radioStable.AutoSize = true;
-            radioStable.Font = new Font("メイリオ", 14F, FontStyle.Regular, GraphicsUnit.Point, 128);
-            radioStable.Location = new Point(120, 350);
-            radioStable.Name = "radioStable";
-            radioStable.Size = new Size(128, 46);
-            radioStable.TabIndex = 7;
-            radioStable.TabStop = true;
-            radioStable.Text = "Stable";
-            radioStable.UseVisualStyleBackColor = true;
+            labelStatusValue.AutoSize = true;
+            labelStatusValue.Font = new Font("メイリオ", 14F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            labelStatusValue.Location = new Point(279, 358);
+            labelStatusValue.Name = "labelStatusValue";
+            labelStatusValue.Size = new Size(98, 42);
+            labelStatusValue.TabIndex = 3;
+            labelStatusValue.Text = "label1";
             // 
-            // radioMobile
+            // comboBoxMode
             // 
-            radioMobile.AutoSize = true;
-            radioMobile.Font = new Font("メイリオ", 14F, FontStyle.Regular, GraphicsUnit.Point, 128);
-            radioMobile.Location = new Point(120, 450);
-            radioMobile.Name = "radioMobile";
-            radioMobile.Size = new Size(130, 46);
-            radioMobile.TabIndex = 8;
-            radioMobile.TabStop = true;
-            radioMobile.Text = "Mobile";
-            radioMobile.UseVisualStyleBackColor = true;
+            comboBoxMode.Font = new Font("メイリオ", 14F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            comboBoxMode.FormattingEnabled = true;
+            comboBoxMode.Location = new Point(250, 41);
+            comboBoxMode.Name = "comboBoxMode";
+            comboBoxMode.Size = new Size(182, 50);
+            comboBoxMode.TabIndex = 4;
+            // 
+            // textBoxSessionId
+            // 
+            textBoxSessionId.Font = new Font("メイリオ", 14F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            textBoxSessionId.Location = new Point(720, 41);
+            textBoxSessionId.Name = "textBoxSessionId";
+            textBoxSessionId.Size = new Size(182, 49);
+            textBoxSessionId.TabIndex = 5;
+            // 
+            // buttonStart
+            // 
+            buttonStart.Font = new Font("メイリオ", 14F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            buttonStart.Location = new Point(1114, 41);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(162, 59);
+            buttonStart.TabIndex = 6;
+            buttonStart.Text = "button1";
+            buttonStart.UseVisualStyleBackColor = true;
+            buttonStart.Click += buttonStart_Click;
+            // 
+            // buttonAudioOn
+            // 
+            buttonAudioOn.Font = new Font("メイリオ", 14F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            buttonAudioOn.Location = new Point(98, 228);
+            buttonAudioOn.Name = "buttonAudioOn";
+            buttonAudioOn.Size = new Size(162, 59);
+            buttonAudioOn.TabIndex = 7;
+            buttonAudioOn.Text = "button1";
+            buttonAudioOn.UseVisualStyleBackColor = true;
+            // 
+            // buttonAudioOff
+            // 
+            buttonAudioOff.Font = new Font("メイリオ", 14F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            buttonAudioOff.Location = new Point(333, 228);
+            buttonAudioOff.Name = "buttonAudioOff";
+            buttonAudioOff.Size = new Size(162, 59);
+            buttonAudioOff.TabIndex = 8;
+            buttonAudioOff.Text = "button1";
+            buttonAudioOff.UseVisualStyleBackColor = true;
+            // 
+            // buttonStop
+            // 
+            buttonStop.Font = new Font("メイリオ", 14F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            buttonStop.Location = new Point(577, 228);
+            buttonStop.Name = "buttonStop";
+            buttonStop.Size = new Size(162, 59);
+            buttonStop.TabIndex = 9;
+            buttonStop.Text = "button1";
+            buttonStop.UseVisualStyleBackColor = true;
+            // 
+            // textBoxLog
+            // 
+            textBoxLog.Font = new Font("メイリオ", 14F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            textBoxLog.Location = new Point(98, 497);
+            textBoxLog.Multiline = true;
+            textBoxLog.Name = "textBoxLog";
+            textBoxLog.Size = new Size(2074, 464);
+            textBoxLog.TabIndex = 10;
             // 
             // Host
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(2287, 1014);
-            Controls.Add(radioMobile);
-            Controls.Add(radioStable);
-            Controls.Add(radioQuality);
-            Controls.Add(radioBalanced);
-            Controls.Add(streamModeDesc);
+            Controls.Add(textBoxLog);
+            Controls.Add(buttonStop);
+            Controls.Add(buttonAudioOff);
+            Controls.Add(buttonAudioOn);
+            Controls.Add(buttonStart);
+            Controls.Add(textBoxSessionId);
+            Controls.Add(comboBoxMode);
+            Controls.Add(labelStatusValue);
+            Controls.Add(labelStatusTitle);
+            Controls.Add(labelSessionId);
+            Controls.Add(labelMode);
             Name = "Host";
             Text = "TwinsRemote Host";
-            Load += Host_Load;
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-        private TextBox streamModeDesc;
-        private RadioButton radioBalanced;
-        private RadioButton radioQuality;
-        private RadioButton radioStable;
-        private RadioButton radioMobile;
+
+        private Label labelMode;
+        private Label labelSessionId;
+        private Label labelStatusTitle;
+        private Label labelStatusValue;
+        private ComboBox comboBoxMode;
+        private TextBox textBoxSessionId;
+        private Button buttonStart;
+        private Button buttonAudioOn;
+        private Button buttonAudioOff;
+        private Button buttonStop;
+        private TextBox textBoxLog;
     }
 }
--- TwinsRemoteHost/TwinsRemoteHost/Form1.cs
+++ TwinsRemoteHost/TwinsRemoteHost/Form1.cs
@@ -1,15 +1,388 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
 namespace TwinsRemoteHost
 {
     public partial class Host : Form
     {
+        private Process? _hostProcess;
+        private LocaleData locale;
+
         public Host()
         {
             InitializeComponent();
+            InitializeUi();
         }
 
-        private void Host_Load(object sender, EventArgs e)
+        private void InitializeUi()
         {
+            comboBoxMode.Items.Clear();
+            comboBoxMode.Items.Add(new VideoPresetItem { DisplayName = "Balanced", Key = "balanced" });
+            comboBoxMode.Items.Add(new VideoPresetItem { DisplayName = "Quality", Key = "quality" });
+            comboBoxMode.Items.Add(new VideoPresetItem { DisplayName = "Stable", Key = "stable" });
+            comboBoxMode.Items.Add(new VideoPresetItem { DisplayName = "Mobile", Key = "mobile" });
+            comboBoxMode.SelectedIndex = 0;
 
+            labelStatusValue.Text = "停止中";
+            SetRunningState(false);
         }
+
+        private void SetRunningState(bool running)
+        {
+            comboBoxMode.Enabled = !running;
+            textBoxSessionId.Enabled = !running;
+            buttonStart.Enabled = !running;
+
+            buttonAudioOn.Enabled = running;
+            buttonAudioOff.Enabled = running;
+            buttonStop.Enabled = running;
+        }
+
+        private void AppendLog(string message)
+        {
+            textBoxLog.AppendText(
+                $"[{DateTime.Now:HH:mm:ss}] {message}{Environment.NewLine}"
+            );
+        }
+
+        private void SetStatus(string status)
+        {
+            labelStatusValue.Text = status;
+        }
+
+        private async void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (_hostProcess != null && !_hostProcess.HasExited)
+            {
+                AppendLog("すでに起動中です");
+                return;
+            }
+
+            string mode = (comboBoxMode.SelectedItem?.ToString() ?? "Balanced").ToLower();
+            string sessionId = textBoxSessionId.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(sessionId))
+            {
+                MessageBox.Show("セッションIDを入力してください", "確認",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string exePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "exes", "twins_remote_host.exe");
+            string exeDir = Path.GetDirectoryName(exePath)!;
+
+            if (!File.Exists(exePath))
+            {
+                MessageBox.Show($"host.exe が見つかりません\n{exePath}", "エラー",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var psi = new ProcessStartInfo
+            {
+                FileName = exePath,
+                WorkingDirectory = exeDir,
+                Arguments = $"--mode {mode} --session \"{sessionId}\"",
+                UseShellExecute = false,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true
+            };
+
+            string signalBaseUrl = "https://play.twins-remote.com";
+            string hostToken = await IssueHostTokenAsync(signalBaseUrl, sessionId);
+
+            psi.Environment["SIGNAL_BASE_URL"] = signalBaseUrl;
+            psi.Environment["WEBRTC_CONFIG_TOKEN"] = hostToken;
+
+            _hostProcess = new Process
+            {
+                StartInfo = psi,
+                EnableRaisingEvents = true
+            };
+
+            _hostProcess.Exited += HostProcess_Exited;
+
+            try
+            {
+                bool started = _hostProcess.Start();
+                if (!started)
+                {
+                    AppendLog("host.exe の起動に失敗しました");
+                    return;
+                }
+
+                AppendLog($"twins_remote_host.exe を起動しました: mode={mode}, session={sessionId}");
+                SetStatus("起動中");
+                SetRunningState(true);
+
+                _ = Task.Run(() => ReadOutputLoop(_hostProcess));
+                _ = Task.Run(() => ReadErrorLoop(_hostProcess));
+            }
+            catch (Exception ex)
+            {
+                AppendLog("起動例外: " + ex.Message);
+                SetStatus("起動失敗");
+                SetRunningState(false);
+            }
+        }
+
+        private void HostProcess_Exited(object? sender, EventArgs e)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new Action(() => HostProcess_Exited(sender, e)));
+                return;
+            }
+
+            AppendLog("twins_remote_host.exe が終了しました");
+            SetStatus("停止中");
+            SetRunningState(false);
+        }
+
+        private async Task ReadOutputLoop(Process process)
+        {
+            try
+            {
+                while (true)
+                {
+                    string? line = await process.StandardOutput.ReadLineAsync();
+                    if (line == null) break;
+
+                    BeginInvoke(new Action(() =>
+                    {
+                        AppendLog("[OUT] " + line);
+                    }));
+                }
+            }
+            catch (Exception ex)
+            {
+                BeginInvoke(new Action(() =>
+                {
+                    AppendLog("標準出力読み取りエラー: " + ex.Message);
+                }));
+            }
+        }
+
+        private async Task ReadErrorLoop(Process process)
+        {
+            try
+            {
+                while (true)
+                {
+                    string? line = await process.StandardError.ReadLineAsync();
+                    if (line == null) break;
+
+                    BeginInvoke(new Action(() =>
+                    {
+                        AppendLog("[ERR] " + line);
+                    }));
+                }
+            }
+            catch (Exception ex)
+            {
+                BeginInvoke(new Action(() =>
+                {
+                    AppendLog("標準エラー読み取りエラー: " + ex.Message);
+                }));
+            }
+        }
+
+        private void HandleOutputLine(string line)
+        {
+            AppendLog("[OUT] " + line);
+
+            if (line.StartsWith("[STATE] "))
+            {
+                string state = line.Substring(8).Trim();
+
+                switch (state)
+                {
+                    case "HOST_STARTING":
+                        SetStatus("起動中");
+                        break;
+                    case "HOST_READY":
+                        SetStatus("待機中");
+                        break;
+                    case "AUDIO_ON":
+                        SetStatus("音声ON");
+                        break;
+                    case "AUDIO_OFF":
+                        SetStatus("音声OFF");
+                        break;
+                    case "EXITING":
+                        SetStatus("終了中");
+                        break;
+                }
+            }
+        }
+
+        private void SendCommand(string command)
+        {
+            if (_hostProcess == null || _hostProcess.HasExited)
+            {
+                AppendLog("プロセスが起動していません。");
+                return;
+            }
+
+            try
+            {
+                _hostProcess.StandardInput.WriteLine(command);
+                _hostProcess.StandardInput.Flush();
+                AppendLog("[CMD] " + command);
+            }
+            catch (Exception ex)
+            {
+                AppendLog("コマンド送信失敗: " + ex.Message);
+            }
+        }
+
+        private void buttonAudioOn_Click(object sender, EventArgs e)
+        {
+            SendCommand("audio on");
+        }
+
+        private void buttonAudioOff_Click(object sender, EventArgs e)
+        {
+            SendCommand("audio off");
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            if (_hostProcess == null || _hostProcess.HasExited)
+            {
+                SetStatus("停止中");
+                SetRunningState(false);
+                return;
+            }
+
+            try
+            {
+                SendCommand("quit");
+
+                if (!_hostProcess.WaitForExit(2000))
+                {
+                    AppendLog("終了応答がないため強制終了します。");
+                    _hostProcess.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                AppendLog("停止時エラー: " + ex.Message);
+            }
+        }
+
+        private async Task<string> IssueHostTokenAsync(string signalBaseUrl, string sessionId)
+        {
+            using var http = new HttpClient();
+
+            string url = signalBaseUrl.TrimEnd('/') + "/issue-host-token";
+
+            var resp = await http.PostAsJsonAsync(url, new IssueHostTokenRequest
+            {
+                SessionId = sessionId
+            });
+
+            resp.EnsureSuccessStatusCode();
+
+            var body = await resp.Content.ReadFromJsonAsync<IssueHostTokenResponse>();
+            if (body == null || string.IsNullOrWhiteSpace(body.Token))
+                throw new Exception("host token response is empty");
+
+            return body.Token;
+        }
+    }
+
+    public class VideoPresetItem
+    {
+        public string DisplayName { get; set; } = "";
+        public string Key { get; set; } = "";
+
+        public override string ToString() => DisplayName;
+    }
+
+    class LocaleData
+    {
+        [JsonObject("Japanese")]
+        public sealed class Japanese
+        {
+            [JsonProperty("labelModeText")]
+            public string labelModeText { get; }
+
+            [JsonProperty("labelSessionIdText")]
+            public string labelSessionIdText { get; }
+
+            [JsonProperty("labelStatusTitleText")]
+            public string labelStatusTitleText { get; }
+
+            [JsonProperty("labelStatusValueText")]
+            public string labelStatusValueText { get; }
+
+            [JsonProperty("buttonStartText")]
+            public string buttonStartText { get; }
+
+            [JsonProperty("buttonAudioOnText")]
+            public string buttonAudioOnText { get; }
+
+            [JsonProperty("buttonAudioOffText")]
+            public string buttonAudioOffText { get; }
+
+            [JsonProperty("buttonAudioStopText")]
+            public string buttonAudioStopText { get; }
+        }
+
+        [JsonObject("English")]
+        public sealed class English
+        {
+            [JsonProperty("labelModeText")]
+            public string labelModeText { get; }
+
+            [JsonProperty("labelSessionIdText")]
+            public string labelSessionIdText { get; }
+
+            [JsonProperty("labelStatusTitleText")]
+            public string labelStatusTitleText { get; }
+
+            [JsonProperty("labelStatusValueText")]
+            public string labelStatusValueText { get; }
+
+            [JsonProperty("buttonStartText")]
+            public string buttonStartText { get; }
+
+            [JsonProperty("buttonAudioOnText")]
+            public string buttonAudioOnText { get; }
+
+            [JsonProperty("buttonAudioOffText")]
+            public string buttonAudioOffText { get; }
+
+            [JsonProperty("buttonAudioStopText")]
+            public string buttonAudioStopText { get; }
+        }
+    }
+
+    public class IssueHostTokenRequest
+    {
+        [JsonPropertyName("sessionId")]
+        public string SessionId { get; set; } = "";
+    }
+
+    public class IssueHostTokenResponse
+    {
+        [JsonPropertyName("token")]
+        public string Token { get; set; } = "";
+
+        [JsonPropertyName("expiresIn")]
+        public int ExpiresIn { get; set; }
     }
 }
